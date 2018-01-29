474be7219870684a66b6eada29c021dc982afd96
Joe Thorngren <joe.thorngren@tackmobile.com>
2018-01-28 20:24:21 -0600

    Use AndroidSupportInjectionModule to inject DispatchingAndroidInjector into BaseApplication.

--- app/src/main/java/com/esoxjem/movieguide/di/AppComponent.kt
+++ app/src/main/java/com/esoxjem/movieguide/di/AppComponent.kt
@@ -1,5 +1,6 @@
 package com.esoxjem.movieguide.di
 
+import com.esoxjem.movieguide.BaseApplication
 import com.esoxjem.movieguide.di.modules.AppModule
 import com.esoxjem.movieguide.di.modules.DetailsModule
 import com.esoxjem.movieguide.di.modules.ListingModule
@@ -8,16 +9,19 @@ import javax.inject.Singleton
 
 
 import dagger.Component
+import dagger.android.support.AndroidSupportInjectionModule
 
 /**
  * @author arunsasidharan
  * @author pulkitkumar
  */
 @Singleton
-@Component(modules = [AppModule::class, NetworkModule::class])
+@Component(modules = [AndroidSupportInjectionModule::class, AppModule::class, NetworkModule::class])
 interface AppComponent {
 
     fun plus(detailsModule: DetailsModule): DetailsComponent
 
     fun plus(listingModule: ListingModule): ListingComponent
+
+    fun inject(application: BaseApplication)
 }
