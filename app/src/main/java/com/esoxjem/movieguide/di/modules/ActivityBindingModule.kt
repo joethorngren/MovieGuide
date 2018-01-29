735130a7dd449afa9b353ee4011a42f588aff587
Joe Thorngren <joe.thorngren@tackmobile.com>
2018-01-29 14:44:08 -0600

    Inject MoviesListingActivity via @ContributesAndroidInjector

--- app/src/main/java/com/esoxjem/movieguide/di/modules/ActivityBindingModule.kt
+++ app/src/main/java/com/esoxjem/movieguide/di/modules/ActivityBindingModule.kt
@@ -1,22 +1,14 @@
 package com.esoxjem.movieguide.di.modules
 
 import com.esoxjem.movieguide.listing.MoviesListingActivity
-import dagger.Binds
 import dagger.Module
-import dagger.android.ActivityKey
-import dagger.multibindings.IntoMap
-import android.app.Activity
-import com.esoxjem.movieguide.di.components.MoviesListingActivitySubcomponent
-import dagger.android.AndroidInjector
+import dagger.android.ContributesAndroidInjector
 
 
-
-@Module(subcomponents = [MoviesListingActivitySubcomponent::class])
+@Module
 abstract class ActivityBindingModule {
 
-    @Binds
-    @IntoMap
-    @ActivityKey(MoviesListingActivity::class)
-    abstract fun bindMovieListingActivityInjectorFactory(builder: MoviesListingActivitySubcomponent.Builder): AndroidInjector.Factory<out Activity>
+    @ContributesAndroidInjector
+    abstract fun bindMovieListingActivityInjectorFactory(): MoviesListingActivity
 
 }
