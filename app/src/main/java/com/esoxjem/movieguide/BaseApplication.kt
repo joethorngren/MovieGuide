b72e0dab2783557d5c184fd71e416946ae6eee89
gerdface <stevenagerdes@gmail.com>
2018-01-29 23:38:59 -0600

    Inject BaseApplication by implementing HasActivityInjector and AndroidInjector.

--- app/src/main/java/com/esoxjem/movieguide/BaseApplication.kt
+++ app/src/main/java/com/esoxjem/movieguide/BaseApplication.kt
@@ -1,5 +1,6 @@
 package com.esoxjem.movieguide
 
+import android.app.Activity
 import android.app.Application
 import com.esoxjem.movieguide.di.AppComponent
 import com.esoxjem.movieguide.di.DaggerAppComponent
@@ -9,19 +10,30 @@ import com.esoxjem.movieguide.di.modules.AppModule
 import com.esoxjem.movieguide.di.modules.DetailsModule
 import com.esoxjem.movieguide.di.modules.ListingModule
 import com.esoxjem.movieguide.di.modules.NetworkModule
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
 
 /**
  * @author arun
  */
-class BaseApplication : Application() {
+class BaseApplication : Application(), HasActivityInjector {
 
-    private var appComponent: AppComponent? = null
+    @Inject lateinit var dispatchingActivityInjector: DispatchingAndroidInjector<Activity>
+
+    private lateinit var appComponent: AppComponent
     private var detailsComponent: DetailsComponent? = null
     private var listingComponent: ListingComponent? = null
 
     override fun onCreate() {
         super.onCreate()
         appComponent = createAppComponent()
+        appComponent.inject(this)
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity> {
+        return dispatchingActivityInjector
     }
 
     private fun createAppComponent(): AppComponent {
@@ -31,13 +43,13 @@ class BaseApplication : Application() {
                 .build()
     }
 
-    fun createDetailsComponent(): DetailsComponent = appComponent!!.plus(DetailsModule())
+    fun createDetailsComponent(): DetailsComponent = appComponent.plus(DetailsModule())
 
     fun releaseDetailsComponent() {
         detailsComponent = null
     }
 
-    fun createListingComponent(): ListingComponent = appComponent!!.plus(ListingModule())
+    fun createListingComponent(): ListingComponent = appComponent.plus(ListingModule())
 
     fun releaseListingComponent() {
         listingComponent = null
--- app/src/main/java/com/esoxjem/movieguide/di/AppComponent.kt
+++ app/src/main/java/com/esoxjem/movieguide/di/AppComponent.kt
@@ -1,21 +1,27 @@
 package com.esoxjem.movieguide.di
 
+
+import com.esoxjem.movieguide.BaseApplication
 import com.esoxjem.movieguide.di.modules.AppModule
 import com.esoxjem.movieguide.di.modules.DetailsModule
 import com.esoxjem.movieguide.di.modules.ListingModule
 import com.esoxjem.movieguide.di.modules.NetworkModule
-import javax.inject.Singleton
-
-
 import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
 
 /**
  * @author arunsasidharan
  * @author pulkitkumar
  */
 @Singleton
-@Component(modules = [AppModule::class, NetworkModule::class])
-interface AppComponent {
+@Component(modules = [
+    AndroidSupportInjectionModule::class,
+    AppModule::class,
+    NetworkModule::class
+])
+interface AppComponent : AndroidInjector<BaseApplication> {
 
     fun plus(detailsModule: DetailsModule): DetailsComponent
 
