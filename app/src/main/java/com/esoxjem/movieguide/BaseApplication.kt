a4e3029b5e3cadf66242ceab7866b8ce1a2f094f
gerdface <stevenagerdes@gmail.com>
2018-01-29 23:46:02 -0600

    Remove @Provides method for BaseApplicaiton and opt for baked-in @BindsInstance.

--- app/src/main/java/com/esoxjem/movieguide/BaseApplication.kt
+++ app/src/main/java/com/esoxjem/movieguide/BaseApplication.kt
@@ -6,10 +6,8 @@ import com.esoxjem.movieguide.di.AppComponent
 import com.esoxjem.movieguide.di.DaggerAppComponent
 import com.esoxjem.movieguide.di.DetailsComponent
 import com.esoxjem.movieguide.di.ListingComponent
-import com.esoxjem.movieguide.di.modules.AppModule
 import com.esoxjem.movieguide.di.modules.DetailsModule
 import com.esoxjem.movieguide.di.modules.ListingModule
-import com.esoxjem.movieguide.di.modules.NetworkModule
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
@@ -37,10 +35,7 @@ class BaseApplication : Application(), HasActivityInjector {
     }
 
     private fun createAppComponent(): AppComponent {
-        return DaggerAppComponent.builder()
-                .appModule(AppModule(this))
-                .networkModule(NetworkModule())
-                .build()
+        return DaggerAppComponent.builder().build()
     }
 
     fun createDetailsComponent(): DetailsComponent = appComponent.plus(DetailsModule())
