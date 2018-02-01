77cbbefd6e26a724b1807519887188f30eb54cca
Joe Thorngren <joe.thorngren@tackmobile.com>
2018-02-01 12:30:33 -0600

    Convert scopes to Kotlin.

---
+++ app/src/main/java/com/esoxjem/movieguide/di/scopes/DetailsScope.kt
@@ -0,0 +1,11 @@
+package com.esoxjem.movieguide.di.scopes
+
+import javax.inject.Scope
+
+
+/**
+ * @author arunsasidharan
+ */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class DetailsScope
